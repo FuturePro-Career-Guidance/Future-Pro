0159ef66bb8bfa5146701d33249f1b55ea8346bb
Milanka00 <milankatharangana01@gmail.com>
2023-07-31 14:42:24 +0530

    new Routes

--- src/Components/navBar.jsx
+++ src/Components/navBar.jsx
@@ -16,32 +16,27 @@ const NavBar = () => {
         <li class="nav-item me-2">
           <a class="nav-link active" aria-current="page" href="/">Home</a>
         </li>
-        <li class="nav-item ms-2 me-2">
-          <a class="nav-link" href="#">About Us</a>
+        <li class="nav-item me-2">
+          <a class="nav-link active" aria-current="page" href="/aboutus">About Us</a>
         </li>
 
-        <li class="nav-item ms-2 me-2">
-          <a class="nav-link" href="#">Services</a>
-        </li>
-
-        <li class="nav-item ms-2 me-2">
-          <a class="nav-link" href="/futurePronet">FuturePro Net</a>
-        </li>
-
-        {/* <li class="nav-item dropdown ms-2 me-2">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+        <li class="nav-item dropdown ms-2 me-2">
+          <a class="nav-link active dropdown-toggle" aria-current="page" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Services
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider" /></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="#">CV Preparation</a></li>
+            <li><a class="dropdown-item" href="#">Interview Preparation</a></li>
           </ul>
-        </li> */}
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
         </li>
+        <li class="nav-item me-2">
+          <a class="nav-link active" aria-current="page" href="futurePronet">FuturePro Net</a>
+        </li>
+
+        <li class="nav-item me-2">
+      <button className='btn btn-primary'>Skill Test</button>
+        </li>
+       
       </ul>
       
     </div>
