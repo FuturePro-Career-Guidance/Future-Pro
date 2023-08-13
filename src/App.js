301518ef217406af7cff70aeb6c2b3a5581121cc
Milanka00 <milankatharangana01@gmail.com>
2023-08-13 07:22:22 +0530

    footer done

--- src/App.js
+++ src/App.js
@@ -4,16 +4,14 @@ import React,{useState} from 'react';
 import {BrowserRouter as Router,Routes,Route,Link } from 'react-router-dom'
 
 
-import Post from './Components/post';
-import PostingPage from './Components/postingC';
 import NetPage from './Pages/futureproNet';
 import LandingPage from './Pages/landingPage';
-import AboutUs from './Pages/aboutUs';
 import CVPrep from './Pages/cvPrep';
 import InterviewPrep from './Pages/Interviewprep';
 import GeneralCareer from './Pages/generalCareer';
 import QuizPage from './Pages/QuizPage';
 import  MyProfile from './Pages/profile'
+import Footer from './InterviewComponents/Footer/Footer';
 
 function App() {
   const [auth,setAuth]=useState(false);
@@ -32,6 +30,8 @@ function App() {
           <Route path='/myProfile' element={<MyProfile/>}/>
         </Routes>
       </Router>
+
+      <Footer/>
 
 
     </div>
